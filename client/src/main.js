f7cc6e594a59c65d9c8aa27a12456ff9c49cc432
Ryan Nugent <ryan.nugent247@gmail.com>
2025-05-22 17:11:30 +0100

    feat: message system and a bit of css

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,60 @@
 console.log("hello world");
+
+// send the message to the database when send is pressed
+const messageForm = document.getElementById("messageform");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(messageForm);
+  const formValues = Object.fromEntries(formData);
+
+  fetch("http://localhost:8080/newMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
+  console.log(formValues);
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+
+//Retreive data and display it in the chat box
+
+async function getMessage() {
+  const response = await fetch("http://localhost:8080/message");
+  const nalaMessages = await response.json();
+  // console.log(nalaMessages);
+  return nalaMessages;
+}
+
+// getMessage();
+
+const messageBox = document.getElementById("user-messages");
+
+function createMessageElements(messageArray) {
+  messageArray.forEach((item) => {
+    const messageName = document.createElement("h3");
+    const messageMessage = document.createElement("p");
+    const messageLikes = document.createElement("p");
+    //get a like image aswell, heart?
+    //make a delete button (Extra)
+
+    messageName.textContent = item.name;
+    messageMessage.textContent = item.message;
+    messageLikes.textContent = item.likes;
+
+    messageBox.appendChild(messageName);
+    messageBox.appendChild(messageMessage);
+    messageBox.appendChild(messageLikes);
+  });
+}
+
+async function renderMessages() {
+  const messageContent = await getMessage();
+  console.log(messageContent);
+  createMessageElements(messageContent);
+}
+renderMessages();
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request } from "express";
 import pg from "pg";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -17,7 +17,23 @@ const db = new pg.Pool({
 app.listen(8080, () => {
   console.log("Server is running on port 8080, yippeeee");
 });
-
+//my root rout
 app.get("/", (request, response) => {
   response.json({ message: "welcome to my server. This is the root route" });
+});
+
+//process data into database
+app.post("/newMessage", (request, response) => {
+  const body = request.body;
+  const query = db.query(
+    `INSERT INTO nalamessages (name, message, likes) VALUES ($1, $2, $3)`,
+    [body.name, body.message, body.likes]
+  );
+  response.json(query);
+});
+
+//read data from the message database
+app.get("/message", async (request, response) => {
+  const query = await db.query("SELECT * FROM nalamessages");
+  const messageData = response.json(query.rows);
 });
